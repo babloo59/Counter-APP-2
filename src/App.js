8b1fdd6b2cc1d5c967f1b263dba71dd2a4d81904
Babloo Kumar <100776753+babloo59@users.noreply.github.com>
2024-11-19 23:33:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="w-full h-screen flex bg-[#344151] items-center justify-center flex-col gap-10">
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
